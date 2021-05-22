94fd14472b9d9d64c2b8246e68c3cc13f348215b
janmaijay1997 <30903817+janmaijay1997@users.noreply.github.com>
2021-05-23 00:38:21 +0530

    Initial commit

---
+++ src/test/java/com/intent/MockitoDemo/MockitoDemo/MockitoDemoApplicationTests.java
@@ -0,0 +1,14 @@
+package com.intent.MockitoDemo.MockitoDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+class MockitoDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+		assertTrue(true);
+	}
+
+}
